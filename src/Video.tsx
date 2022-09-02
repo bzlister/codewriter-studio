3300979c0588b6be1e17793bb5df154f42d89470
Ben <bzlister@gmail.com>
2022-09-01 22:13:02 -0400

    state exploration

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,17 +1,37 @@
-import {Composition} from 'remotion';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
+import {Composition, Sequence, useCurrentFrame, useVideoConfig} from 'remotion';
 import {Workspace} from './compositions/workspace/workspace';
 import {defaultConfig} from '../workspace.config';
 import {ContextProvider} from './context-provider';
+
+const useOdd = (frame: number) => {
+	const oddCount = useRef(0);
+	if (frame % 2 === 1) {
+		oddCount.current += 1;
+	}
+	return {props: oddCount.current, done: oddCount.current >= 10};
+};
+
+const StatefulComponent = () => {
+	const {height, width} = useVideoConfig();
+	const frame = useCurrentFrame();
+
+	const odds = usePureComposition(frame, useOdd);
+
+	return (
+		<div
+			style={{background: 'white', textAlign: 'center', height, width}}
+		>{`${odds} odds in ${frame}`}</div>
+	);
+};
 
 export const RemotionVideo: React.FC = () => {
 	return (
 		<Composition
 			id="MyComp"
-			component={() => (
-				<ContextProvider {...defaultConfig}>{Workspace}</ContextProvider>
-			)}
-			durationInFrames={5000}
-			fps={40}
+			component={() => <StatefulComponent />}
+			durationInFrames={500}
+			fps={1}
 			width={1280}
 			height={720}
 		/>
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { useContext, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useCurrentFrame, useVideoConfig } from "remotion"
 import { Context } from "./context-provider";
 
@@ -9,4 +9,19 @@ export const useTypewriter = (length: number) => {
   const { charsPerSecond } = useContext(Context);
 
   return Math.min(Math.round(((frame - start.current) * charsPerSecond) / fps), length);
-} 
+}
+
+export const useControl = (play: boolean) => {
+	const frame = useCurrentFrame();
+	const previousFrame = useRef<number>(frame);
+	const virtual = useRef(0);
+
+	if (previousFrame.current !== frame) {
+		if (play) {
+			virtual.current += frame - previousFrame.current;
+		}
+		previousFrame.current = frame;
+	}
+
+	return virtual.current;
+};
